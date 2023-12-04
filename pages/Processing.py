0e219b2952ace6e5a72942c2295d5e1513ac88c9
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2023-12-05 03:04:29 +0700

    Update Processing.py

--- pages/Processing.py
+++ pages/Processing.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
 
-df = pd.read_csv('weather.csv')
+df = pd.read_csv('finaldata.csv')
 
 st.title(''' PENGOLAHAN DATA LAPORAN CUACA DI SEATTLE DENGAN ALGORITMA KNN ''')
 st.write('Prediksi Data Baru')
@@ -28,4 +28,15 @@ st.number_input ("Wind Velocity")
 #max_value=df('precipation').max()
 
 st.button("Predict")
+
+X = df.iloc[:, :-1]
+y = df['weather']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+
 st.subheader("Prediction : ")
